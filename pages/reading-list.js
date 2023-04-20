c3d9114f4996c95034f2f7bda63704b76f7fec71
bos-hieu <12520839@gm.uit.edu.vn>
2023-04-20 23:14:54 +0700

    add company

--- pages/reading-list.js
+++ pages/reading-list.js
@@ -1,16 +1,35 @@
-import Link from "@/components/Link";
+import Link from '@/components/Link'
 
 export default function ReadingList() {
-    return (
-        <>
-            <ul>
-                <li>
-                    <Link href="https://learn.mongodb.com/learning-paths/mongodb-python-developer-path">Mongodb python developer path</Link>
-                </li>
-                <li>
-                    <Link href="https://levelup.gitconnected.com/use-git-like-a-senior-engineer-ef6d741c898e">Use git like a senior engineer</Link>
-                </li>
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href="https://learn.mongodb.com/learning-paths/mongodb-python-developer-path">
+            Mongodb python developer path
+          </Link>
+        </li>
+        <li>
+          <Link href="https://levelup.gitconnected.com/use-git-like-a-senior-engineer-ef6d741c898e">
+            Use git like a senior engineer
+          </Link>
+        </li>
+        <li>
+          <Link href="https://careers.adyen.com">
+            Adyen
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.checkout.com/careers">
+            Checkout.com
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.uber.com/us/en/careers/">
+            Uber
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
 }
