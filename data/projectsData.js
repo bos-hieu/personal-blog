c0d7133bd40c2e704b664f971531e5fa40ff69f2
bos-hieu <12520839@gm.uit.edu.vn>
2024-04-20 21:00:18 -0600

    feat: update project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,12 +1,28 @@
 const projectsData = [
   {
-    title: 'Autonomous Inc Website Latest',
+    title: 'Autonomous Inc Website Latest Version',
     description:
       'Contributing as a Backend Developer to the third version of the autonomous.ai website — an e-commerce site. This version marks a massive transition from monolithic to microservices architecture, enhancing scalability for approximately 1 million monthly visitors. Notably, the admin site has been migrated from Python Flask to Vue.js, while the frontend continues to leverage ReactJS.',
     // href: "https://www.autonomous.ai",
     deployed: 'https://www.autonomous.ai',
     tools: ['Golang', 'GRPC', 'Microservice', 'MongoDB', 'MYSQL', 'Redis', 'Google Cloud'],
     imgSrc: '/static/images/projectCards/autonomous.png',
+  },
+  {
+    title: 'Ecommerce Order Purchasing Smart Contract',
+    description:
+        'Developed a smart contract for an e-commerce order purchasing system. This contract allows users to purchase products using Ethereum cryptocurrency. The contract is deployed on the Sepolia testnet and interacts with a frontend web application. This project is made for educational purposes.',
+    tools: ['Solidity', 'ReactJS', 'NextJS', 'Truffle', 'Ganache', 'Metamask', "Web3.js"],
+    deployed: 'https://ecommerce-order-purchasing-frontend.vercel.app',
+    imgSrc: '/static/images/projectCards/ecommerce-order-purchasing-smart-contract.png',
+  },
+  {
+    title: 'TripX',
+    description:
+        'As Co-founder and Technical Lead, I spearheaded the development of TripX — a car self-driver rental marketplace connecting car owners with renters. This multifaceted project involved creating a frontend web, an admin site, and a mobile app.',
+    tools: ['PHP', 'Laravel', 'Python', 'ReactJS', 'Nginx'],
+    deployed: 'https://play.google.com/store/apps/details?id=com.tripx&pli=1',
+    imgSrc: '/static/images/projectCards/tripx.png',
   },
   {
     title: 'Autonomous Inc Website Version 2',
@@ -21,15 +37,7 @@ const projectsData = [
       'As a Web Developer, I played a crucial role in developing the initial version of the autonomous.ai website. This version utilized a Python Flask framework, combining backend, frontend, and admin site in a single source code.',
     tools: ['Python', 'Flask', 'Jquery', 'Javascript', 'AWS'],
     imgSrc: '/static/images/projectCards/autonomous_v1.png',
-  },
-  {
-    title: 'TripX',
-    description:
-      'As Co-founder and Technical Lead, I spearheaded the development of TripX — a car self-driver rental marketplace connecting car owners with renters. This multifaceted project involved creating a frontend web, an admin site, and a mobile app.',
-    tools: ['PHP', 'Laravel', 'Python', 'ReactJS', 'Nginx'],
-    deployed: 'https://play.google.com/store/apps/details?id=com.tripx&pli=1',
-    imgSrc: '/static/images/projectCards/tripx.png',
-  },
+  }
 ]
 
 export default projectsData
